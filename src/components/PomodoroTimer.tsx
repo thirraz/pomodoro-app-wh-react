d9ba77252803b03ddf5fda3873bdf211a2f24d44
Thiago Souza <thiagofernandosouza20@gmail.com>
2022-12-21 00:06:23 -0300

    button & timer components created

--- src/components/PomodoroTimer.tsx
+++ src/components/PomodoroTimer.tsx
@@ -4,11 +4,14 @@ import { Button } from "./Button"
 import { Timer } from "./Timer"
 
 interface Props {
-     defaultPomodoroTime: number
+     pomodoroTime: number
+     shortRestTime: number
+     longRestTime: number
+     cycles: number
 }
 
 export const PomodoroTimer = (props: Props): JSX.Element => {
-     const [mainTime, setMainTime] = useState(props.defaultPomodoroTime)
+     const [mainTime, setMainTime] = useState(props.pomodoroTime)
 
      useInterval(() => {
           setMainTime(mainTime - 1)
@@ -17,7 +20,18 @@ export const PomodoroTimer = (props: Props): JSX.Element => {
           <div className="pomodoro">
                <h2>You are: working</h2>
                <Timer mainTime={mainTime} />
-               <Button text="teste" onClick={() => console.log(1)} />
+               <div className="controls">
+                    <Button text="teste" onClick={() => console.log(1)} />
+                    <Button text="teste" onClick={() => console.log(1)} />
+                    <Button text="teste" onClick={() => console.log(1)} />
+               </div>
+
+               <div className="details">
+                    <p>ALSDASCNASOICNASOCNAOSCNAOISCNOIAS</p>
+                    <p>ALSDASCNASOICNASOCNAOSCNAOISCNOIAS</p>
+                    <p>ALSDASCNASOICNASOCNAOSCNAOISCNOIAS</p>
+                    <p>ALSDASCNASOICNASOCNAOSCNAOISCNOIAS</p>
+               </div>
           </div>
      )
 }
